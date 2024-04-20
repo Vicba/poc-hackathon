605dcd4a0e51e60fae73c4d000e98d1667ecaf15
Gevers Kelvin <kelvin.gevers@student.howest.be>
2024-04-20 07:53:11 +0200

    add login code

--- client/app/page.js
+++ client/app/page.js
@@ -4,14 +4,38 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
-    const handleClick = () => {
-        console.log("clicked");
+        const connectWallet = async () => {
+      if (typeof window.ethereum !== 'undefined') {
+        try {
+          const accounts = await window.ethereum.request({
+            method: 'eth_requestAccounts',
+          })
+
+          if (accounts.length === 0) {
+            console.log('No account connected')
+          } else {
+            const web3 = new Web3(window.ethereum)
+            await accountStore.login()  
+          }
+        } catch (error) {
+          console.error('Error connecting wallet:', error.message)
+        }
+      } else {
+        console.log('Ethereum provider not found')
+      }
+    }
+
+    const disconnectWallet = () => {
+      accountStore.logout()
+      store.commit("logout");
     }
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
-            <Button onClick={handleClick}>Login</Button>
-
+            {/* {!store.isLoggedIn?(
+            <Button onClick={connectWallet}>Login</Button>
+            ): (<Button onClick={disconnectWallet}>Logout</Button>)} */}
+            <Button onClick={connectWallet}>Login</Button>
             <div className="mt-10">
                 <Link href="/test-score" className="underline text-gray-400 hover:text-gray-700 underline-offset-4">
                     Test score
