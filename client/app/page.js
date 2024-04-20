b56a65d513b8c8533a3fac5b542b94acbaf4630f
Gevers Kelvin <kelvin.gevers@student.howest.be>
2024-04-20 17:04:34 +0200

    add leaderboard

--- client/app/page.js
+++ client/app/page.js
@@ -75,7 +75,7 @@ export default function Home() {
                 </Link>
             </div>
             <Mintnft/>
-            <Overview/>
+            {/* <Overview/> */}
         </main>
     );
 }
